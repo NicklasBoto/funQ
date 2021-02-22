e1a83c248f8d1bfe684d89e364787e24b4ebd657
Marcus Jörgensson <marcusjorgensson@gmail.com>
2021-02-22 16:57:07 +0100

    Add example folder and coinflip example

---
+++ examples/CoinFlip.hs
@@ -0,0 +1,19 @@
+module CoinFlip where
+
+import QM
+import Funqy
+import Gates
+
+
+-- | Given two values, will return either of them with a 50% probability.
+
+-- Example usage:
+--   :l examples/CoinFlip.hs -- Load the haskell file
+--   run $ coinFlip "hard brackets" "parenthesis"
+
+coinFlip :: a -> b -> QM (Either a b)
+coinFlip left right = do
+    q <- new 0
+    q <- hadamard q
+    b <- measure q
+    return $ if b == 0 then Left left else Right right
