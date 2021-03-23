94d15f853fd960c190ba963ebacb720c833a8de5
Fabian Forslund <fabfors@student.chalmers.se>
2021-03-23 15:27:14 +0100

    interpretor works a little better
    Co-authored by: @rudsberg
    Co-authored by: @beataburreau

--- src/Interpreter/Interpreter.hs
+++ src/Interpreter/Interpreter.hs
@@ -12,12 +12,21 @@ import Control.Monad.Identity
 import qualified FunQ as Q
 import Control.Monad.State
 import Parser.Abs as Abs
-    ( Gate(GS, GH, GX, GY, GZ, GI, GT),
+    ( Gate(GS, GH, GX, GY, GZ, GI, GT, GCNOT, GTOF, GSWP, GFRDK),
       Type,
       Bit(BOne, BZero),
       Program )
 import qualified AST.AST as A
 import Parser.Par              (pProgram, myLexer)
+
+-- TODO:
+-- fixa errors 
+-- teleportera 1, och få 1
+-- eval för custom run gate (vart/hur definierar ens användaren sina egna gates?)
+-- generell run gate???
+-- show för VFunc? 
+-- main driver: egen fil? turtle och haskelline? 
+-- test suite
 
 type Named = Int
 data Error
@@ -38,23 +47,17 @@ data Ctx = Ctx {
     , functions :: Sig      -- Maps function identifiers to terms
 }
 
-data Value
-    = VBit Q.Bit
-    | VQBit Q.QBit
-    | VUnit
-    | VTup [Value]
-    | VFunc [Value] A.Term
-    
+
 instance Show Value where
     show (VBit b)    = show b
     show (VTup bs)   = show bs
     show (VQBit q)   = show q
-    show VUnit       = "*"
-    show (VFunc t e)     = "nope"
+    show  VUnit      = "*"
+    show (VFunc t e) = "nope"
     -- show (VFunc vs e) = case runCheck (typecheck (Abs t e)) of
     --     Right t' -> show t'
 
--- Main function in interpreter (which we export )
+-- Main function in interpreter (which we export)
 interpret :: [A.Function] -> Eval Value
 interpret fs = do
     -- Create context 
@@ -73,111 +76,93 @@ getMainTerm ctx = case M.lookup "main" (functions ctx) of
     Just term -> term
     Nothing   -> error "No main function" -- TODO: use our own errors
 
-runInterpreter :: Eval a -> ExceptT Value IO a
-runInterpreter st =  undefined
+data Value
+    = VBit Q.Bit
+    | VQBit Q.QBit
+    | VUnit
+    | VTup [Value]
+    | VFunc [Value] A.Term -- could [Value] be removed?
 
--- exceptToIO :: Eval Value -> IO (Either Value a)
--- exceptToIO  = liftIO . runExceptT . runInterpreter
-
-eval :: Ctx -> A.Term -> Eval Value     -- ??
+eval :: Ctx -> A.Term -> Eval Value     
 eval ctx = \case
     A.Idx j -> return $ values ctx !! fromInteger j
 
-    -- A.QVar s -> qbits?
+    A.QVar s -> case M.lookup s (functions ctx) of
+        Just t  -> eval ctx t
+        Nothing -> error "qbit?"  -- Must be a free variable? qbit?
 
     A.Bit BZero -> return $ VBit 0
     A.Bit BOne -> return $ VBit 1
 
     A.Gate g -> error "Using gate on nothing" -- TODO: correct errors
 
+    A.Tup bs -> VTup <$> mapM (eval ctx) bs
+
     A.App (A.Gate g) q -> case g of
-         Abs.GH    -> runGate Q.hadamard q ctx
-         Abs.GX    -> runGate Q.pauliX q ctx
-         Abs.GY    -> runGate Q.pauliY q ctx
-         Abs.GZ    -> runGate Q.pauliZ q ctx
-         Abs.GI    -> runGate Q.identity q ctx
-         Abs.GT    -> runGate Q.tdagger q ctx
-         Abs.GS    -> runGate Q.phase q ctx
-        --Abs.GCNOT -> runGate Q.cnot q ctx
-        --Abs.GTOF  -> runGate Q.toffoli q ctx
-        --Abs.GSWP  -> runGate Q.swap q ctx
-        --Abs.GFRDK -> runGate Q.fredkin q ctx
+         Abs.GH    -> runGate  Q.hadamard q ctx
+         Abs.GX    -> runGate  Q.pauliX q ctx
+         Abs.GY    -> runGate  Q.pauliY q ctx
+         Abs.GZ    -> runGate  Q.pauliZ q ctx
+         Abs.GI    -> runGate  Q.identity q ctx
+         Abs.GT    -> runGate  Q.tdagger q ctx
+         Abs.GS    -> runGate  Q.phase q ctx
+         Abs.GCNOT -> run2Gate Q.cnot q ctx
+         Abs.GTOF  -> run3Gate Q.toffoli q ctx
+         Abs.GSWP  -> run2Gate Q.swap q ctx
+         Abs.GFRDK -> run3Gate Q.fredkin q ctx
         -- GGate GateIdent
+        
+    A.App A.New b -> do
+        VBit b' <- eval ctx b
+        q <- lift $ Q.new b'
+        return $ VQBit q
+
+    A.App A.Meas q -> do
+        VQBit q' <- eval ctx q
+        b <- lift $ Q.measure q' 
+        return $ VBit b
+
+    A.App e1 e2 -> do 
+        VFunc _ a <- eval ctx e1
+        v <- eval ctx e2
+        eval ctx{ values = v : values ctx } a
+
+    A.IfEl bit l r -> do
+        VBit b <- eval ctx bit 
+        eval ctx $ if b == 1 then l else r
+        
+    A.Let eq inn -> do 
+        VTup [x1, x2] <- eval ctx eq 
+        eval ctx{ values = x2 : x1 : values ctx } inn
+
+    A.Abs e -> return $ VFunc (values ctx) e
+
+    A.Void -> return VUnit
+
+    A.New -> error "New should not be found by itself"
+    
+    A.Meas -> error "Meas should not be found by itself"
 
 
+-- TODO: refactor more generally!
 runGate :: (Q.QBit -> Q.QM Q.QBit) -> A.Term -> Ctx -> Eval Value
 runGate g q ctx = do
-    res <- eval ctx q 
-    case res of
-        VQBit q' -> VQBit <$> lift (g q')
-        _        -> error ""            -- TODO: correct errors
-    
+    VQBit q' <- eval ctx q 
+    VQBit <$> lift (g q')
 
+run2Gate :: ((Q.QBit, Q.QBit) -> Q.QM (Q.QBit, Q.QBit)) -> A.Term -> Ctx -> Eval Value
+run2Gate g q ctx = do
+    VTup [VQBit a, VQBit b] <- eval ctx q
+    res <- lift $ g (a,b)
+    return $ VTup (tupToList res)
+        where tupToList (a,b) = [VQBit a,VQBit b]
 
--- runCNOT :: A.Term -> Ctx -> Eval Value
--- runCNOT q ctx = do
---     VTup [VQBit a, VQBit b] <- eval ctx q
---     let (c,d) = Q.cnot (a, b)
---     return $ VBit 0
-
-    -- A.Void -> return VUnit
-
-    -- A.Tup bs -> VTup <$> mapM (eval ctx) bs
-
-    -- one = \f.\x.f x
-    -- suc = \n.\f.\x.n f (f x)
-    -- eval $ suc one
-    -- suc[n := one] -\beta> 
-    -- \f.\x. (\f.\x.f x ) f (f x)
-    -- \f.\x. f (f x)
-
-    -- A.Abs e -> return $ VFunc (values ctx) e
-
-    -- A.App A.New b -> do
-        -- VBit b' <- eval ctx b
-        -- q <- lift $ Q.new b'
-        -- return $ VQBit q
-
-   -- A.App A.Meas q -> do
-   --     VQBit q' <- eval ctx q
-   --     b <- lift $ Q.measure q' 
-   --     return $ VBit b
---
-
-   --     
-   -- A.App e1 e2 -> do   -- EXAMPLE: f 10   0 -> 10  f --> \0
-   --     VFunc e a <- eval ctx e1
-   --     v <- eval ctx e2
-   --     eval ctx{values = v : values} a
---
-   -- A.IfEl bit l r -> do
-   --     VBit b <- eval ctx bit 
-   --     eval ctx $ if b == 1 then l else r
-   -- 
-   -- A.Let eq inn -> do 
-   --     VTup [x1, x2] <- eval ctx eq 
-   --     eval ctx{values = x2 : x1 : values} inn
---
-   -- _ -> throwError $ Fail "not implemented"
-
---  case t of
---          Bit b -> return $ VBit b
-
--- | A term in our intermediatary representation.
--- data Term
---    = Idx  Integer       -- bound
---    | QVar String        -- free
---    | Bit  Bit
---    | Gate Gate
---    | Tup  [Term]
---    | App  Term Term
---    | IfEl Term Term Term
---    | Let Term Term
---    | Abs  Term
---    | New
---    | Meas
---    | Void
-
+run3Gate :: ((Q.QBit, Q.QBit, Q.QBit) -> Q.QM (Q.QBit, Q.QBit, Q.QBit)) -> A.Term -> Ctx -> Eval Value
+run3Gate g q ctx = do 
+    VTup [VQBit a, VQBit b, VQBit c] <- eval ctx q
+    res <- lift $ g (a,b,c)
+    return $ VTup (tupToList res)
+        where tupToList (a,b,c) = [VQBit a, VQBit b, VQBit c] 
 
 
 -- Should be in a main pipeline file
@@ -188,7 +173,7 @@ run fileName = do
     res <- Q.run $ runExceptT $ interpret (A.toIm program)
     case res of
         Left err -> do
-            putStrLn "INTERPRETER ERROR "
+            putStrLn "INTERPRETER ERROR"
             error ""
         Right i -> do
             putStrLn $ "Result " ++ show i
