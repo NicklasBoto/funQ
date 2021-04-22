4f869d96a7ffecf8c842f2e2f822c1cc8eabb55b
Joel Rudsberg <rudsberg@live.se>
2021-04-22 14:39:34 +0200

    Semantic checks for duplicate functions, that main exist and that function names match
    Co-authored-by: beataburreau <beataburreau@users.noreply.github.com>

--- src/SemanticAnalysis/SemanticAnalysis.hs
+++ src/SemanticAnalysis/SemanticAnalysis.hs
@@ -1,6 +1,6 @@
 {-# language LambdaCase #-}
 
-module SemanticAnalysis where
+module SemanticAnalysis.SemanticAnalysis where
 
 import qualified FunQ as Q
 import qualified AST.AST as A
@@ -19,23 +19,74 @@ import Control.Exception (try)
 import qualified Type.HM as HM
 import Data.List
 import Parser.Abs
+import qualified Data.Set as Set
 
 data SemanticError 
-    = FunNameMismatch String
+  = FunNameMismatch String
+  | NoMainFunction String
+  | DupFun String
+  | UnknownGate String
+    deriving Show
 
----- Semantic analysis
--- flytta ut i egen fil
--- error som e fint :) 
 semanticAnalysis :: Program -> Either SemanticError ()
-semanticAnalysis p = funNameMatch p >>= anotherCheck 
+semanticAnalysis p = dupFun p -- funNameMatch p -- >>= mainDefined p 
+
 
 funNameMatch :: Program -> Either SemanticError ()
-funNameMatch p@(PDef fs) = if (all check fs) then return p else throwError $ FunNameMismatch ""
-  where check :: FunDec -> Bool
-        check (FDecl (FunVar s) _ (FDef (Var s') _ _)) = (funName s) == s'
-        funName :: String -> String 
-        funName s = takeUntil " " (takeUntil ":" s)
+funNameMatch p@(PDef fs) = if null mismatches then Right () else Left $ FunNameMismatch $ "Mismatchig names in function declaration and definition for " ++ mismatches -- mapM_ check fs
+  where mismatches = concat $ intersperse ", " $ check fs []
+        check :: [FunDec] -> [String] -> [String]   
+        check [] ms = ms
+        check ((FDecl (FunVar s) _ (FDef (Var s') _ _)):fs) ms = if (funName' s) == s' then check fs ms else check fs (ms ++ [(funName' s)])  
+        funName' :: String -> String 
+        funName' s = takeUntil " " (takeUntil ":" s)
+    
 
+mainDefined :: Program -> Either SemanticError ()
+mainDefined (PDef fs) = if any isMain fs then Right () else Left $ NoMainFunction "No main function has been declared"
+  where isMain f = funName f == "main"
+
+
+
+dupFun :: Program -> Either SemanticError ()
+dupFun (PDef fs) = if null collectErrors then Right () else Left $ DupFun $ "Duplicate functions declarations for " ++ collectErrors
+  where collectErrors = concat $ intersperse ", " $ check funNames []
+        check :: [String] -> [String] -> [String]
+        check []     errors = errors 
+        check (f:fs) errors = if dup f && (not $ elem f fs) then check fs (f:errors) else check fs errors
+        funNames = map funName fs
+        dup :: String -> Bool
+        dup f = length (filter (== f) funNames) > 1
+        
+
+-- gateIdent blir fail! 
+unknownGate :: Program -> Either SemanticError ()
+unknownGate (PDef fs) = undefined -- then Right () else Left $ UnknownGate ""
+  where check :: [FunDec] -> [String] -> [String]
+        check [] gs = gs
+        check ((FDecl _ _ (FDef _ _ t)):fs) gs = if null $ unknownGates t then check fs gs else check fs (gs ++ unknownGates t)
+        unknownGates :: Term -> [String]
+        unknownGates = undefined
+
+      --   data Term
+      --   = TVar Var
+      --   | TBit Bit
+      --   | TGate Gate
+      --   | TTup Tup
+      --   | TStar
+      --   | TApp Term Term
+      --   | TIfEl Term Term Term
+      --   | TLet LetVar [LetVar] Term Term
+      --   | TLamb Lambda Var Term
+      -- deriving (C.Eq, C.Ord, C.Show, C.Read)
+
+-- Endast 0/1 (måste göra om grammar!)
+
+
+-- antalet argument är samma eller fler som antalet typer 
+
+
+-- Utils
 takeUntil :: String -> String -> String
 takeUntil [] [] = []                           --don't need this
 takeUntil xs [] = [] 
@@ -44,20 +95,5 @@ takeUntil xs (y:ys) = if isPrefixOf xs (y:ys)
                       then []
                       else y:(takeUntil xs (tail (y:ys)))
 
--- check main exist (remove main check from interpreter)
-
-
--- duplicate function names (se typcheckaren)
-
-
--- gateIdent blir fail! 
-
-
--- Endast 0/1 (måste göra om grammar!)
-
-
--- antalet argument är samma eller fler som antalet typer 
-
-
-anotherCheck :: Program -> Either SemanticError ()
-anotherCheck p = return ()
+funName :: FunDec -> String 
+funName (FDecl _ _ (FDef (Var s) _ _)) = s
