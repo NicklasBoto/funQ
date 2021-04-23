d60a670671c3ee41bb86462ce23c56987ba42652
Joel Rudsberg <rudsberg@live.se>
2021-04-23 10:28:38 +0200

    Clean up main
    Co-authored-by: beataburreau <beataburreau@users.noreply.github.com>

--- src/Interpreter/Main.hs
+++ src/Interpreter/Main.hs
@@ -51,7 +51,7 @@ main = runInputT defaultSettings loop
                 liftIO $ runIO "src/AST/test.fq" -- (w !! 1)
                 loop
               _ -> do
-                liftIO $ runTerminalIO $ "main : a main = " ++ input
+                liftIO $ runTerminal $ "main : a main = " ++ input
                 loop
 
 type Run a = ExceptT Error IO a
@@ -81,43 +81,24 @@ instance Show Error where
   show (NoSuchFile f) =
     "file not found: " ++ f
 
+-- Run
 runIO :: FilePath -> IO ()
 runIO path = runExceptT (run path) >>= \case
   Left  err -> putStrLn $ "*** Exception, " ++ show err
   Right val -> print val
 
-toErr :: (i -> Either e v) -> (e -> Error) -> (v -> o) -> i -> Run o
-toErr f l r = ExceptT . return . bimap l r . f
-
---run :: FilePath -> Run I.Value
---run path = readfile path >>= parse >>= typecheck >>= eval
-
-run :: FilePath -> Run I.Value
-run path = readfile path >>= parse' >>= semanticAnalysis >>= convertAST >>= typecheck >>= eval
- 
-runTerminalIO :: String -> IO ()
-runTerminalIO s = runExceptT (runTerminal s) >>= \case
+runTerminal :: String -> IO ()
+runTerminal s = runExceptT (run s) >>= \case
   Left  err -> putStrLn $ "*** Exception, " ++ show err
   Right val -> putStrLn $ "Output: " ++ show val
 
-runTerminal :: String -> Run I.Value
-runTerminal s = parse s >>= typecheck >>= eval
+runFile :: FilePath -> Run I.Value
+runFile path = readfile path >>= run
 
-readfile :: FilePath -> Run String
-readfile path = do
-  e <- liftIO (try (readFile path) :: IO (Either IOError String))
-  case e of
-    Left  _ -> throwError $ NoSuchFile path
-    Right s -> return s
+run :: String -> Run I.Value
+run s = parse s >>= semanticAnalysis >>= convertAST >>= typecheck >>= eval
 
-parse :: String -> Run A.Program
-parse = toErr (pProgram . myLexer) ParseError A.toIm
-
-parse' :: String -> Run Program
-parse' s = case (pProgram $ myLexer s) of 
-  Left err  -> throwError $ ParseError "gick mindre bra" -- todo error
-  Right b   -> return b
-
+-- Components
 convertAST :: Program -> Run A.Program
 convertAST = return . A.toIm 
 
@@ -130,6 +111,22 @@ eval = withExceptT ValueError . mapExceptT Q.run . I.interpret
 semanticAnalysis :: Program -> Run Program 
 semanticAnalysis = toErr S.runAnalysis SemanticError . const <*> id
 
+-- Utils 
+toErr :: (i -> Either e v) -> (e -> Error) -> (v -> o) -> i -> Run o
+toErr f l r = ExceptT . return . bimap l r . f
+
+readfile :: FilePath -> Run String
+readfile path = do
+  e <- liftIO (try (readFile path) :: IO (Either IOError String))
+  case e of
+    Left  _ -> throwError $ NoSuchFile path
+    Right s -> return s
+
+parse :: String -> Run Program
+parse s = case (pProgram $ myLexer s) of 
+  Left err  -> throwError $ ParseError err
+  Right b   -> return b
+
 rundistest :: FilePath -> IO ()
 rundistest path = do
   res <- runExceptT $ rundist path
@@ -139,20 +136,11 @@ rundistest path = do
 
 rundist :: FilePath -> Run [I.Value]
 rundist path = do 
-  a <- readfile path >>= parse >>= typecheck
+  a <- readfile path >>= parse >>= semanticAnalysis >>= convertAST >>= typecheck
   evaldist a 300
-
 
 evaldist :: A.Program -> Int -> Run [I.Value]
 evaldist prg reps = replicateM reps $ (withExceptT ValueError . mapExceptT Q.run . I.interpret) prg
---   where 
-
-        -- sortera listan 
-        -- -> plocka ut unika element 
-        -- -> räkna antalet av de olika unika elementen i startlistan
-        -- -> konvertera till binärt
-        -- -> printa andelar 
--- input (1,0,1)
 
 exVTup :: I.Value
 exVTup = I.VTup (I.VBit 1) (I.VTup (I.VBit 0) (I.VBit 1))
