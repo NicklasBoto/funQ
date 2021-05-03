9bc10b213c6fcf576c0ff8e7bc394a2ea8afc852
Fabian Forslund <fabfors@student.chalmers.se>
2021-05-03 10:43:03 +0200

    run programs with auto-generated inputs

--- src/Interpreter/Main.hs
+++ src/Interpreter/Main.hs
@@ -38,7 +38,7 @@ main = runInputT defaultSettings loop
           Just ":help" -> outputStrLn ":q to quit, :run filename to run a file, type to expressions" >> loop
           Just input -> do
             let w = words input
-            case head w of 
+            case head w of
               ":run" -> do
                 case length w of
                   1 -> outputStrLn "Need to specify file to run" >> loop
@@ -123,11 +123,11 @@ toErr :: (i -> Either e v) -> (e -> Error) -> (v -> o) -> i -> Run o
 toErr f l r = ExceptT . return . bimap l r . f
 
 parse :: String -> Run Program
-parse s = case (pProgram $ myLexer s) of
+parse s = case pProgram (myLexer s) of
   Left err  -> throwError $ ParseError err
   Right b   -> return b
 
--- | Runs a file some number of times.
+-- | Distribution runs of programs
 rundistest :: FilePath -> Int -> IO ()
 rundistest path runs = do
   res <- runExceptT $ rundist path runs
@@ -172,9 +172,64 @@ stat len ((a,b):as) = (a, dub b/dub len, b) : stat len as
   where dub = fromIntegral . toInteger
 
 prettystats :: Int -> (Int, Double, Int) -> String
-prettystats len (a,b,c) = concatMap show ((fillzeros . toBin) a) ++ ": " ++ "\t" ++ (show . truncateboi) b ++ "%" ++ "\t" ++ show c
-  where toBin 0 = []
-        toBin n | n `mod` 2 == 1 = toBin (n `div` 2) ++ [1]
-        toBin n | n `mod` 2 == 0 = toBin (n `div` 2) ++ [0]
-        truncateboi d = (fromIntegral . truncate) (10000*(d :: Double))/100
-        fillzeros as = if length as == len then as else replicate (len - length as) 0 ++ as
+prettystats len (a,b,c) = concatMap show ((fillzeros len . toBin) a) ++ ": " ++ "\t" ++ (show . truncateboi) b ++ "%" ++ "\t" ++ show c
+  where truncateboi d = (fromIntegral . truncate) (10000*(d :: Double))/100
+        
+toBin :: Int -> [Int]
+toBin 0 = []
+toBin n | n `mod` 2 == 1 = toBin (n `div` 2) ++ [1]
+toBin n | n `mod` 2 == 0 = toBin (n `div` 2) ++ [0]
+
+fillzeros :: Int -> [Int] -> [Int]
+fillzeros len as = if length as == len then as else replicate (len - length as) 0 ++ as
+
+-- kör program med index 1 -> sätt tillbaka in1,in2,... -> kör om med index 2
+
+-- | Run program with generated inputs
+runNewInputs :: FilePath -> IO ()
+runNewInputs path = do
+  file <- readFile path
+  let ixs = inds file
+  res <- runExceptT $ mapM (evalNewInputs ixs file) [0..7]
+  case res of
+    Left err -> putStrLn $ "*** Exception:, " ++ show err
+    Right r  -> gatherResults r
+
+evalNewInputs :: [Int] -> String -> Int ->  Run I.Value
+evalNewInputs ixs prg inputNr = do
+  let newfile = updateIns 0 ixs (inputsNew 3 !! inputNr) prg in parse newfile >>= semanticAnalysis >>= convertAST >>= typecheck >>= eval
+
+updateIns :: Int -> [Int] -> [String] -> String -> String
+updateIns _ [] _ prg = prg
+updateIns n (ix:ixs) (input:inputs) prg
+  | n == ix = updateIns (n+1) (map (+1) ixs) inputs (replaceIx prgs (ix + 1) input)
+  | otherwise = updateIns (n+1) (ix:ixs) (input:inputs) prg
+  where prgs = words prg
+
+replaceIx :: [String] -> Int -> String -> String
+replaceIx list ix val  = let (a,b) = splitAt ix list in
+    let elem = last a in unwords $ take (ix-1) a ++ mend elem val : b
+    where mend elem val
+            | head elem == '(' = '(' : val ++ ","
+            | last elem == ')' = val ++ ")"
+            | otherwise = val ++ ","
+
+inds :: String -> [Int]
+inds l = findIndices predN (words l)
+  where predN :: String -> Bool
+        predN s = let clean = filter (\x -> x `elem` ['a'..'z'] || x `elem` ['1'..'9']) s in
+                  clean `elem` ins
+
+file :: [String]
+file =  ["main",":","(Bit","><","Bit","><","Bit)","main","=","let","(a,b,c)","=","TOFFOLI","(in1,","in2,","in3)","in","(meas","a,","meas","b,","meas","c)"]
+
+inputsNew :: Int -> [[String]]
+inputsNew len = let bins = map (fillzeros len . toBin) (concat (replicate 4 [6,7])) in [map ((++) "new " . show) a | a <- bins]
+
+ins :: [String]
+ins = ["in" ++ show a | a <- [1..9]]
+
+-- testinds :: IO ()
+-- testinds = do
+--   res <- readFile "test/interpreter-test-suite/toffoli.fq"
+--   print $ updateIns 0 (inds res) inputsNew res
