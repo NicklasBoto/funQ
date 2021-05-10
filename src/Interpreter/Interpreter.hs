54b2cdf1497dedea2cabcbdfe91f32dc344006fc
Beata Burreau <burreau@student.chalmers.se>
2021-05-10 18:31:45 +0200

    Refactored eval

--- src/Interpreter/Interpreter.hs
+++ src/Interpreter/Interpreter.hs
@@ -20,8 +20,8 @@ instance Show ValueError where
 
 
 type Sig = M.Map String A.Term
-type TopLevel = M.Map String Value
-type Eval = ExceptT ValueError (StateT TopLevel Q.QM)
+type FunctionValues = M.Map String Value
+type Eval = ExceptT ValueError (StateT FunctionValues Q.QM)
 
 -- | Environment type, stores bound variables & functions
 data Env = Env {
@@ -69,11 +69,20 @@ getMainTerm env = case M.lookup "main" (functions env) of
 
 -- | Term evaluator
 eval :: Env -> A.Term -> Eval Value
-eval env = \case
-    A.Idx j -> if j >= fromIntegral (length (values env)) then throwError (Fail ("Index" ++ show j ++ "is too large"))
-             else return $ values env !! fromIntegral j
+eval env (A.Bit A.BZero) = return $ VBit 0
+eval env (A.Bit A.BOne)  = return $ VBit 1
+eval env (A.Abs t e)     = return $ VAbs (values env) t e
+eval env (A.Unit)        = return VUnit
+eval env (A.Gate g)      = return $ VGate g
+eval env (A.New)         = return VNew
+eval env (A.Meas)        = return VMeas
 
-    A.Fun s -> case M.lookup s (functions env) of
+eval env (A.Idx j) = if j >= fromIntegral (length (values env)) 
+    then throwError (Fail ("Index" ++ show j ++ "is too large"))
+    else return $ values env !! fromIntegral j
+
+eval env (A.Fun s) = do 
+    case M.lookup s (functions env) of
         Just t  -> do
             fs <- get
             case M.lookup s fs of
@@ -84,43 +93,41 @@ eval env = \case
                     return v
         Nothing -> throwError $ NotFunction s
 
-    A.Bit A.BZero -> return $ VBit 0
-    A.Bit A.BOne -> return $ VBit 1
+eval env (A.Tup t1 t2) = do
+    v1 <- eval env t1
+    v2 <- eval env t2
+    return $ VTup v1 v2
 
-    A.Tup t1 t2 -> do
-        v1 <- eval env t1
-        v2 <- eval env t2
-        return $ VTup v1 v2
-
-    A.App e1 e2 -> case e1 of
+eval env (A.App t1 t2) = do
+    case t1 of
         A.Gate g -> case g of
-            A.GH      -> runGate  Q.hadamard e2 env
-            A.GX      -> runGate  Q.pauliX e2 env
-            A.GY      -> runGate  Q.pauliY e2 env
-            A.GZ      -> runGate  Q.pauliZ e2 env
-            A.GI      -> runGate  Q.identity e2 env
-            A.GT      -> runGate  Q.phasePi8 e2 env
-            A.GS      -> runGate  Q.phase e2 env
-            A.GCNOT   -> run2Gate Q.cnot e2 env
-            A.GTOF    -> run3Gate Q.toffoli e2 env
-            A.GSWP    -> run2Gate Q.swap e2 env
-            A.GFRDK   -> run3Gate Q.fredkin e2 env
-            A.GQFT n  -> runQFT   (Q.qft n) e2 env
-            A.GQFTI n -> runQFT   (Q.qftDagger n) e2 env
-            A.GCR n   -> run2Gate (`Q.cphase` (1/(n*2))) e2 env
-            A.GCRI n  -> run2Gate (`Q.cphase` (-1/(n*2))) e2 env
-            A.GCCR n  -> run3Gate (`Q.ccphase` (1/(n*2))) e2 env
-            A.GCCRI n  -> run3Gate (`Q.ccphase` (-1/(n*2))) e2 env
+            A.GH      -> runGate  Q.hadamard t2 env
+            A.GX      -> runGate  Q.pauliX t2 env
+            A.GY      -> runGate  Q.pauliY t2 env
+            A.GZ      -> runGate  Q.pauliZ t2 env
+            A.GI      -> runGate  Q.identity t2 env
+            A.GT      -> runGate  Q.phasePi8 t2 env
+            A.GS      -> runGate  Q.phase t2 env
+            A.GCNOT   -> run2Gate Q.cnot t2 env
+            A.GTOF    -> run3Gate Q.toffoli t2 env
+            A.GSWP    -> run2Gate Q.swap t2 env
+            A.GFRDK   -> run3Gate Q.fredkin t2 env
+            A.GQFT n  -> runQFT   (Q.qft n) t2 env
+            A.GQFTI n -> runQFT   (Q.qftDagger n) t2 env
+            A.GCR n   -> run2Gate (`Q.cphase` (1/(n*2))) t2 env
+            A.GCRI n  -> run2Gate (`Q.cphase` (-1/(n*2))) t2 env
+            A.GCCR n  -> run3Gate (`Q.ccphase` (1/(n*2))) t2 env
+            A.GCCRI n  -> run3Gate (`Q.ccphase` (-1/(n*2))) t2 env
 
         A.New -> do
-            VBit b' <- eval env e2
+            VBit b' <- eval env t2
             lift $ lift $ VQBit <$> Q.new b'
         A.Meas -> do
-            VQBit q' <- eval env e2
+            VQBit q' <- eval env t2
             lift $ lift $ VBit <$> Q.measure q'
         _ -> do
-            v2 <- eval env e2
-            v1 <- eval env e1
+            v2 <- eval env t2
+            v1 <- eval env t1
             case v1 of
                 VAbs vs _ a -> eval env{ values = v2 : vs ++ values env } a
                 VNew -> eval env{ values = v2 : values env } (A.App A.New (A.Idx 0))
@@ -128,19 +135,13 @@ eval env = \case
                 (VGate g) -> eval env{ values = v2 : values env } (A.App (A.Gate g) (A.Idx 0))
                 _ -> throwError $ Fail $ "Can't apply " ++ show v1 ++ " with " ++ show v2
 
-    A.IfEl bit l r -> do
-        VBit b <- eval env bit
-        eval env $ if b == 1 then l else r
+eval env (A.IfEl t t1 t2) = do
+    VBit b <- eval env t
+    eval env $ if b == 1 then t1 else t2
 
-    A.Let eq inn -> do
-         VTup x1 x2 <- eval env eq
-         eval env{ values = x2 : x1 : values env } inn
-
-    A.Abs t e  -> return $ VAbs (values env) t e
-    A.Unit     -> return VUnit
-    A.Gate g   -> return $ VGate g
-    A.New      -> return VNew
-    A.Meas     -> return VMeas
+eval env (A.Let eq inn) = do
+        VTup v1 v2 <- eval env eq
+        eval env{ values = v2 : v1 : values env } inn
 
 fromVTup :: Value -> [Value]
 fromVTup (VTup a b) = a : fromVTup b
